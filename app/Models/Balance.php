23521287cfd153b49a6faa80960a3d3dc5cae4ad
rafaadail <rafaadailph@gmail.com>
2019-01-18 09:58:41 -0200

    beginTransaction

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Balance extends Model
 {
@@ -10,30 +11,39 @@ class Balance extends Model
 
     public function deposit(float $value) : Array
     {
-        $totalBefore = $this->amount ? $this->amount : 0 ;
+        DB::beginTransaction();
+
+        $totalBefore = $this->amount ?: 0;
         $this->amount += number_format($value, 2, '.', '');
         $deposit = $this->save();
 
         $historic = auth()->user()->historics()->create([
             'type' => 'I',
             'amount' => $value,
-            'total_before' => $totalBefore ,
+            'total_before' => $totalBefore,
             'total_after' => $this->amount,
             'date' => date('Ymd')
         ]);
 
-        if($deposit && $historic)
+        if ($deposit && $historic) {
+
+            DB::commit();
+
             return [
                 'success' => true,
                 'message' => 'Sucesso ao recarregar'
 
             ];
+        }
 
+        if (!$deposit || !$historic) {
+            DB::rollBack();
 
-        return [
-            'success' => false,
-            'message' => 'Falha ao carregar'
+            return [
+                'success' => false,
+                'message' => 'Falha ao carregar'
 
-        ];
+            ];
+        }
     }
 }
