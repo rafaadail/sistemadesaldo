34f74a69cae62c365579d5efae5981f33040428a
rafaadail <rafaadailph@gmail.com>
2019-01-31 22:13:54 -0200

    Preparar Página de Exibição do Perfil

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ $this->group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'adm
     $this->get('/', 'AdminController@index')->name('admin.home');
 });
 
+$this->get('meu-perfil', 'Admin\UserController@profile')->name('profile')->middleware('auth');
 
 $this->get('/', 'Site\SiteController@index')->name('home');
 
