896fbb98cc8a8f5e9f57eb763482053fa1afcee3
rafaadail <rafaadailph@gmail.com>
2019-02-03 00:27:26 -0200

    Criar Template do Site

---
+++ resources/views/site/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
--- resources/views/site/profile/profile.blade.php
+++ resources/views/site/profile/profile.blade.php
@@ -1,1 +1,35 @@
-#profile
+@extends('site.layouts.app')
+
+@section('title', 'Meu Perfil')
+
+@section('content')
+
+<h1>Meu Perfil</h1>
+
+
+<form action="{{ route('profile.update') }}" method="POST">
+    {!! csrf_field() !!}
+    <div class="form-group">
+        <label for="name">Nome</label>
+        <input type="text" value="{{ auth()->user()->name }}" name="name" placeholder="Nome" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" value="{{ auth()->user()->email }}" name="email" placeholder="E-mail" class="form-control">
+
+    </div>
+    <div class="form-group">
+        <label for="password">Senha</label>
+        <input type="text" name="password" placeholder="Senha" class="form-control">
+
+    </div>
+    <div class="form-group">
+        <label for="image">Imagem</label>
+        <input type="file" name="file" placeholder="Imagem" class="form-control">
+
+    </div>
+    <div class="form-group">
+        <button class="btn btn-info ">Atualizar Perfil</button>
+    </div>
+</form>
+@endsection
